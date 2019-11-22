b514eb1633f578bc02bc98d03f6603690a15bfef
GilangFajarRamadhan <aominedaikusuke@gmail.com>
2019-11-22 15:43:27 +0700

    JAVA-PERK9-A2.1900073-GILANGFAJARRAMADHAN

---
+++ latihanjava/1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Gilang Fajar Ramadhan
+ * NIM   : A2.1900073
+ * Kelas : TI-1C
+ * 
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t           : Gilang Fajar Ramadhan ");
+        System.out.println("ALAMAT\t\t         : Tegal Kalong ");
+        System.out.println("TEMPAT LAHIR\t\t : Sumedang ");
+        System.out.println("TANGGAL LAHIR\t\t : 1 Januari 2000");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Membaca");
+        System.out.println("MAKES\t\t: Nasgor");
+        System.out.println("MIKES\t\t: kopi");
+    }
+    
+}
---
+++ latihanjava/3.Memasukan Nilai Dari Keyboard/src/l3/memasukan/nilai/dari/keyboard/L3MemasukanNilaiDariKeyboard.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l3.memasukan.nilai.dari.keyboard;
+
+import java.util.Scanner;
+
+/**
+ * NAMA  : Gilang Fajar Ramadhan
+ * NIM   : A2.1900073
+ * Kelas : TI-1C
+ * 
+ */
+public class L3MemasukanNilaiDariKeyboard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+                
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah " +nama);
+    }
+    
+}
